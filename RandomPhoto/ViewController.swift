5191984b8b88c65ba6e2b65fc590eebea95c2371
musasyncs <rubato.cw@gmail.com>
2021-08-03 15:19:29 +0800

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by Ewen on 2021/8/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
